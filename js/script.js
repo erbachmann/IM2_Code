b0f36e648b97239a46a13a476e8bf973e240ba49
erbachmann <eric.bachmann@stud.fhgr.ch>
2026-05-06 14:14:33 +0200

    Category and Recipe generation implemented

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
-console.log("blub");
+const button = document.querySelector('#button');
+const catecory_selection = document.querySelector('#catecory_kitchen');
+
+// Kategorien laden
+async function loadCatecory() {
+    const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        data.categories.forEach(element => {
+            const option = document.createElement('option');
+            option.value = element.strCategory;
+            option.textContent = element.strCategory;
+            catecory_selection.appendChild(option);
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+loadCatecory();
+
+// Button klick
+button.addEventListener('click', async function () {
+    let url = '';
+    let selectedRecipe;
+
+    if (catecory_selection.value === 'all') {
+        url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    } else {
+        url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${catecory_selection.value}`;
+    }
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        //Random recipe selection
+        if (catecory_selection.value === 'all') {
+            selectedRecipe = (data.meals[0]);
+        } else {
+            const randomIndex = Math.floor(Math.random() * data.meals.length);
+            selectedRecipe = (data.meals[randomIndex]);
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+    console.log(selectedRecipe);
+});
